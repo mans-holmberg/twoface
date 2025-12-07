c2c5be5dcade44522314c932ebe7091cbda0a449
mans-holmberg <holmberg.mans@gmail.com>
2025-12-07 08:40:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,22 @@
 from setuptools import setup, Extension
 import numpy
-
-try:
-    from Cython.Build import cythonize
-    USE_CYTHON = True
-except ImportError:
-    USE_CYTHON = False
-
-ext_src = "c_src/_twoface.pyx" if USE_CYTHON else "c_src/_twoface.c"
+from Cython.Build import cythonize
 
 extensions = [
     Extension(
         "twoface._twoface",
-        sources=[ext_src, "c_src/twoface.c"],
+        sources=["c_src/_twoface.pyx", "c_src/twoface.c"],
         include_dirs=["c_src", numpy.get_include()],
         extra_compile_args=["-O3", "-ffast-math"],
     )
 ]
-
-if USE_CYTHON:
-    extensions = cythonize(extensions)
 
 setup(name="twoface",
     version="0.1.0",
     author="Måns Holmberg",
     packages=["twoface"],
     description="Transit lightcurves for asymmetric planets transiting spotted stars",
-    ext_modules=extensions,
+    ext_modules=cythonize(extensions),
     install_requires=["numpy"],
     zip_safe=False,
 )
